9b45f29d64eb9d6d54fb139e441e31f925ef3753
Aleksandar Ristevski <nice@ristevski.me>
2019-01-22 07:33:33 +0000

    Add Test.jsx React page and CV Json format for test

---
+++ src/pages/test.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { graphql } from 'gatsby';
+import Sidebar from '../components/Sidebar';
+import Layout from '../components/Layout';
+
+class Test extends React.Component {
+  render() {
+    return (
+      <Layout>
+        <div>
+          <Sidebar {...this.props} />
+          <div>
+            <h1>Test</h1>
+          </div>
+        </div>
+      </Layout>
+    );
+  }
+}
+
+export default Test;
+
+export const pageQuery = graphql`
+  query TestQuery {
+    site {
+      siteMetadata {
+        title
+        subtitle
+        copyright
+        menu {
+          label
+          path
+        }
+        author {
+          name
+          email
+          telegram
+          twitter
+          github
+          rss
+        }
+      }
+    }
+  }
+`;
