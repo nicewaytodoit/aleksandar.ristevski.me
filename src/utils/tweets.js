69f165bb2637d53f0ac0d3aa8b562d22c628874a
Aleksandar Ristevski <nice@ristevski.me>
2021-01-10 06:52:05 +0000

    Twitter Social fix not loading

--- src/utils/tweets.js
+++ src/utils/tweets.js
@@ -1,12 +1,13 @@
+const TWITTER_TAG = 'TwitterPlatform';
+
 const loadTwitterEmbed = (callback) => {
-    const existingScript = document.getElementById('TweeterPlatform');
+    const existingScript = document.getElementById(TWITTER_TAG);
     if (existingScript) {
         existingScript.parentNode.removeChild(existingScript);
     }
-
     const script = document.createElement('script');
     script.src = 'https://platform.twitter.com/widgets.js';
-    script.id = 'TwitterPlatform';
+    script.id = TWITTER_TAG;
     script.charSet = 'utf-8';
     document.body.appendChild(script);
     script.onload = () => {
